02bc951d01a5aa60435fcd7799e8fdda15bd3b3f
mozahid <aymanmozahid@gmail.com>
2023-04-27 17:08:56 +0600

    uodare incom

--- src/app/all-products/all-products.component.ts
+++ src/app/all-products/all-products.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { product } from '../model/product';
 import { map } from 'rxjs';
+import { __values } from 'tslib';
 
 @Component({
   selector: 'app-all-products',
@@ -14,6 +15,7 @@ export class AllProductsComponent implements OnInit {
 
   allproducts:product[] = [];
   isFeatching:boolean = false;
+  isEdit:boolean = false;
 
   ngOnInit() {
     this.fetchData();
@@ -32,6 +34,25 @@ export class AllProductsComponent implements OnInit {
     .subscribe((res)=> {
       this.productFetch();
     })
+  }
+
+  onEdit(id: string) {
+    // Get product based on id
+    let currentProduct = this.allproducts.find((p) => {return p.id === id} )
+    console.log(currentProduct);
+    this.isEdit = true;
+
+    // Populated the form with the form details
+
+    // Change the button value to update product
+
+
+
+  }
+  onUpdate(id:any, value:product) {
+    this.http.put(`https://angularfirst-3d22c-default-rtdb.firebaseio.com/products/${id}.json`, value)
+    .subscribe()
+  
   }
 
 
---
+++ src/app/services/product.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({providedIn:'root'})
+export class productService {
+
+    createProduct() {
+
+    }
+
+    fetchProduct() {
+
+    }
+
+    deleteproduct() {
+
+    }
+
+    deleteAllProduct() {
+        
+    }
+
+}
